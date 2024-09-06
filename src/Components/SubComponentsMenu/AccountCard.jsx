1046b127aa095d6d946a9ec01b12d1bb1b6e2253
Daniel Pizarro Saavedra <Saavedra.Alejandro@outlook.com>
2024-09-05 21:44:53 -0400

    Actualización Responsive Fake X

--- src/Components/SubComponentsMenu/AccountCard.jsx
+++ src/Components/SubComponentsMenu/AccountCard.jsx
@@ -9,7 +9,7 @@ export function AccountCard() {
                   <strong><span className='lateral-menu-user-name'>Danielpizarro_</span></strong>
                   <span className='lateral-menu-user-account'>@Danielpizarros_</span>
                 </div>
-                <button style={{position: 'relative', left: '60px', top: '10px'}} className='feed-post-option'>
+                <button className='feed-post-option'>
                   <svg className='feed-post-option-svg' viewBox="0 0 24 24" aria-hidden="true"><g><path d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path></g></svg>
                 </button>
               </div>
